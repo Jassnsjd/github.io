13a3237f5577e8d4f0e5247dce7f7480d32f7ff7
Jassn <dnfyxms@yeah.net>
2020-11-19 18:59:59 +0800

    add

---
+++ react-big-screen/mock/centerPageData.js
@@ -0,0 +1,266 @@
+// 中间界面数据
+// let colorList = [[
+//   '#ff7f50', '#87cefa', '#da70d6', '#32cd32', '#6495ed',
+//   '#ff69b4', '#ba55d3', '#cd5c5c', '#ffa500', '#40e0d0',
+//   '#1e90ff', '#ff6347', '#7b68ee', '#d0648a', '#ffd700',
+//   '#6b8e23', '#4ea397', '#3cb371', '#b8860b', '#7bd9a5'
+// ],
+// [
+//   '#ff7f50', '#87cefa', '#da70d6', '#32cd32', '#6495ed',
+//   '#ff69b4', '#ba55d3', '#cd5c5c', '#ffa500', '#40e0d0',
+//   '#1e90ff', '#ff6347', '#7b68ee', '#00fa9a', '#ffd700',
+//   '#6b8e23', '#ff00ff', '#3cb371', '#b8860b', '#30e0e0'
+// ],
+// [
+//   '#929fff', '#9de0ff', '#ffa897', '#af87fe', '#7dc3fe',
+//   '#bb60b2', '#433e7c', '#f47a75', '#009db2', '#024b51',
+//   '#0780cf', '#765005', '#e75840', '#26ccd8', '#3685fe',
+//   '#9977ef', '#f5616f', '#f7b13f', '#f9e264', '#50c48f'
+// ]][2];
+module.exports = {
+  'GET /api/centerPageData': {
+
+    detailsList: [{
+      "name": "公司代码比赛参加参赛情况",
+      "value": 2373,
+      "symbolSize": 48,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#929fff',
+          "color": '#929fff'
+        }
+      }
+    }, {
+      "name": "公司单身男同胞统计",
+      "value": 5449,
+      "symbolSize": 73,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#9de0ff',
+          "color": '#9de0ff'
+        }
+      }
+    }, {
+      "name": "公司单身小仙女统计",
+      "value": 2289,
+      "symbolSize": 67,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#ffa897',
+          "color": '#ffa897'
+        }
+      }
+    }, {
+      "name": "数据统计系统开发进度",
+      "value": 2518,
+      "symbolSize": 50,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#af87fe',
+          "color": '#af87fe'
+        }
+      }
+    }, {
+      "name": "年轻人脱单指南",
+      "value": 4730,
+      "symbolSize": 88,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#7dc3fe',
+          "color": '#7dc3fe'
+        }
+      }
+    }, {
+      "name": "妨害疫情防控的违法行为",
+      "value": 1952,
+      "symbolSize": 55,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#bb60b2',
+          "color": '#bb60b2'
+        }
+      }
+    }, {
+      "name": "46岁老者8天加班",
+      "value": 926,
+      "symbolSize": 70,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#433e7c',
+          "color": '#433e7c'
+        }
+      }
+    }, {
+      "name": "别让快递小哥一直等在寒风中",
+      "value": 4536,
+      "symbolSize": 67,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#f47a75',
+          "color": '#f47a75'
+        }
+      }
+    }, {
+      "name": "最强王者奖花落谁家",
+      "value": 750,
+      "symbolSize": 47,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#009db2',
+          "color": '#009db2'
+        }
+      }
+    }, {
+      "name": "今年光棍节怎么过",
+      "value": 493,
+      "symbolSize": 82,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#024b51',
+          "color": '#024b51'
+        }
+      }
+    }, {
+      "name": "双12购物指南",
+      "value": 385,
+      "symbolSize": 59,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#0780cf',
+          "color": '#0780cf'
+        }
+      }
+    }, {
+      "name": "双11购物指南统计",
+      "value": 4960,
+      "symbolSize": 90,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#765005',
+          "color": '#765005'
+        }
+      }
+    }, {
+      "name": "请假人数统计",
+      "value": 8694000,
+      "symbolSize": 134,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#e75840',
+          "color": '#e75840'
+        }
+      }
+    }, {
+      "name": "12项疫情防控政策",
+      "value": 5668,
+      "symbolSize": 75,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#26ccd8',
+          "color": '#26ccd8'
+        }
+      }
+    }, {
+      "name": "战疫一线的别样团圆",
+      "value": 339,
+      "symbolSize": 68,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#3685fe',
+          "color": '#3685fe'
+        }
+      }
+    }, {
+      "name": "疫情救助站",
+      "value": 671,
+      "symbolSize": 62,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor":'#9977ef',
+          "color": '#9977ef'
+        }
+      }
+    }, {
+      "name": "元旦亮灯为节日加油",
+      "value": 27000,
+      "symbolSize": 114,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#f5616f',
+          "color": '#f5616f'
+        }
+      }
+    }, {
+      "name": "抗击新型肺炎我们在行动",
+      "value": 10777000,
+      "symbolSize": 130,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#f7b13f',
+          "color": '#f7b13f'
+        }
+      }
+    }, {
+      "name": "疫情中的逆行者",
+      "value": 92000,
+      "symbolSize": 123,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#f9e264',
+          "color": '#f9e264'
+        }
+      }
+    }, {
+      "name": "一张图看懂新冠肺炎",
+      "value": 20000,
+      "symbolSize": 141,
+      "draggable": true,
+      "itemStyle": {
+        "normal": {
+          "shadowBlur": 100,
+          "shadowColor": '#50c48f',
+          "color":'#50c48f'
+        }
+      },
+      "url": "https://gallery.echartsjs.com/preview.html?c=xPLngHx_Z&v=1"
+    }]
+  },
+};
